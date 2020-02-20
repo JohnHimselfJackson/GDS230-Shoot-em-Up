ac21b4866c7d55d147e96aa27adfcf06900c5094
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2020-02-20 12:13:43 +0800

    Menu + Navigation + Effects
    Added menu UI + Functionality & Navigation

---
+++ GDS230 Shoot'em Up/Assets/Scripts/OnMouseOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class OnMouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse Entered!");
+        transform.localScale += new Vector3(0.1F, 0, 0);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Mouse Exit!");
+        transform.localScale -= new Vector3(0.1F, 0, 0);
+    }
+}
---
+++ GDS230 Shoot'em Up/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject optionsMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+        }
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        GameIsPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void OptionsResume()
+    {
+        optionsMenuUI.SetActive(false);
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Options()
+    {
+        optionsMenuUI.SetActive(true);
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+}
