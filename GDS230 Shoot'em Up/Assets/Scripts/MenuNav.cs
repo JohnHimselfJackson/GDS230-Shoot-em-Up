f7d4d473098e222c8e68b69a458f125886c4dd1c
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2020-02-25 14:08:53 +0800

    UI + GameController + MenuNav
    -Optimization for Scenes
    -Optimization for Code

---
+++ GDS230 Shoot'em Up/Assets/Scripts/MenuNav.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNav : MonoBehaviour
+{
+    public int i;
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Handles Scene Changes
+    public void MenuNavigator()
+    {
+        SceneManager.LoadScene(i, LoadSceneMode.Single);
+    }
+}
---
+++ GDS230 Shoot'em Up/Assets/Scripts/UIBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBehaviour : MonoBehaviour
+{
+    public void Embiggen(Transform myObject)
+    {
+        Debug.Log("Mouse Entered!");
+        myObject.transform.localScale += new Vector3(0.1F, 0, 0);
+    }
+
+    public void Smally(Transform myObject)
+    {
+        Debug.Log("Mouse Exit!");
+        myObject.transform.localScale -= new Vector3(0.1F, 0, 0);
+    }
+}
