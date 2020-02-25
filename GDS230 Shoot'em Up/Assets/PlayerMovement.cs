a94bfb0157624436e8ea65cb162f5c3dd4a9a7eb
MacVP <48820988+MacVP@users.noreply.github.com>
2020-02-25 09:22:53 +0800

    Test Scene
    yeah not cool man

---
+++ GDS230 Shoot'em Up/Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public Joystick joystick;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    void Update()
+    {
+        horizontalMove = joystick.Horizontal * runSpeed;
+
+        //Joystick up
+        float verticalMove = joystick.Vertical;
+
+        //If joystick is pointing up then jump is true
+        if (verticalMove >= .5f)
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Character move & jump
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
