69d131ad012c974884ad5cb13878772684178dbe
MacVP <48820988+MacVP@users.noreply.github.com>
2020-02-25 11:18:59 +0800

    Test Scene

---
+++ Character Controller (Side Scroller)/Assets/PlayerShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform muzzle;
+
+    public bool shoot;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Space"))
+        {
+            shoot = true;
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, muzzle.position, muzzle.rotation);
+    }
+}
