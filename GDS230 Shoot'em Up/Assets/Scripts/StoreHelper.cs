d62a75513f48117441440078fd608025a218356d
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2020-03-04 02:54:45 +0800

    StoreSystem + Customize Options
    -Added a store system with 1 available customization for purchase
    -Added In game currency (ZEL)

---
+++ GDS230 Shoot'em Up/Assets/Scripts/StoreHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreHelper : MonoBehaviour
+{
+    public static int ZELAmount;
+    public Text ZELText;
+    int isGreenSold;
+    public GameObject green;
+
+    void Start()
+    {
+        ZELAmount = PlayerPrefs.GetInt("ZELAmount");
+        isGreenSold = PlayerPrefs.GetInt("IsGreenSold");
+
+        if (isGreenSold == 1/* Will Need another check to make sure that green is selected in the customize settings yaaaay */)
+        {
+            green.SetActive(true);
+        }
+        else
+        {
+            green.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        ZELText.text = "ZEL: " + ZELAmount.ToString();
+    }
+
+    public void SaveMoney()
+    {
+        PlayerPrefs.SetInt("ZELAmount", ZELAmount);
+    }
+}
--- GDS230 Shoot'em Up/Assets/Scripts/StorePreferences.cs
+++ GDS230 Shoot'em Up/Assets/Scripts/StorePreferences.cs
@@ -1,8 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StorePreferences : MonoBehaviour
 {
-    
+    int isGreenSold;
+    int ZELAmount;
+
+    public Text ZELAmountText;
+    public Text greenSkinPrice;
+
+    public Button greenButton;
+
+    void Start()
+    {
+        ZELAmount = PlayerPrefs.GetInt("ZELAmount");
+    }
+
+    void Update()
+    {
+        ZELAmountText.text = "ZEL: " + ZELAmount.ToString();
+
+        isGreenSold = PlayerPrefs.GetInt("IsGreenSold");
+
+        if (ZELAmount >= 500 && isGreenSold == 0)
+        {
+            greenButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            greenButton.gameObject.SetActive(false);
+        }
+    }
+
+    public void BuyGreenSkin()
+    {
+        ZELAmount -= 500;
+        PlayerPrefs.SetInt("IsGreenSold", 1);
+        greenSkinPrice.text = "Sold!";
+        greenButton.gameObject.SetActive(false);
+    }
+
+    public void ExitShop()
+    {
+        PlayerPrefs.SetInt("ZELAmount", ZELAmount);
+    }
+
+    public void ResetPlayerPrefs()
+    {
+        ZELAmount = 0;
+        greenButton.gameObject.SetActive(true);
+        greenSkinPrice.text = "ZEL: 500";
+        PlayerPrefs.DeleteAll();
+    }
 }
